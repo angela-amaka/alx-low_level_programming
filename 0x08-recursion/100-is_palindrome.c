e663119960c1161408dc6f8f8e0425d26cef264d
angela-amaka <ezeangelachiamaka@gmail.com>
2022-09-29 15:52:25 -0700

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -30,8 +30,10 @@ void check(char *s, int start, int end, int *flag)
 	if (start <= end)
 	{
 		if (s[start] == s[end])
+			*flag *= 1;
+		else
 			*flag *= 0;
-		check(s, start + 1, end -1, flag);
+		check(s, start + 1, end - 1, flag);
 	}
 }
 
