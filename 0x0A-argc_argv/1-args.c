bc54640a768d76216f3d1abad3b56785ad3e8ba8
angela-amaka <ezeangelachiamaka@gmail.com>
2022-09-30 18:24:33 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - entry poit
+ * @argc: count arguments
+ * @argv: arguments
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argv; /*Ignore argv*/
+	printf("%i\n", argc - 1);
+
+	return (0);
+}
